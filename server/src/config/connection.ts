300eb39aca41b7c4f2b6cd86a8379c1016d16ced
Joshua <joshuapruitt6484@gmail.com>
2024-10-11 19:11:02 -0700

    added server.ts apollo code

--- server/src/config/connection.ts
+++ server/src/config/connection.ts
@@ -1,5 +1,18 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/googlebooks');
+const MONGODB_URI = process.env.MONGODB_URI || '';
 
-export default mongoose.connection;
+const db = async (): Promise<typeof mongoose.connection> => {
+    try {
+        await mongoose.connect(MONGODB_URI);
+        console.log('Database Connected.')
+        return mongoose.connection;
+    } catch (err: any){
+        console.error('Database Connection Error:', err);
+        throw new Error('Database connection Failed.');
+    }
+};
+
+export default db;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,21 +1,69 @@
 import express from 'express';
+import { Request, Response } from 'express';
 import path from 'node:path';
+import dbOld from './config/connectionOld.js';
 import db from './config/connection.js';
 import routes from './routes/index.js';
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+//new apollo server import 
+import { ApolloServer } from '@apollo/server';
+import { expressMiddleware } from '@apollo/server/express4';
+import {typeDefs, resolvers} from './schemas/index.js';
+import { authenticateToken } from './services/auth.js';
 
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+const server = new ApolloServer({
+  typeDefs,
+  resolvers
+})
 
-// if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
-}
+const startApolloServer = async () => {
+  await server.start();
+  await db();
 
-app.use(routes);
+  const PORT = process.env.PORT || 3001;
+  const app = express();
+  
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
 
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
-});
+  app.use('/graphql', expressMiddleware(server as any,
+    {
+      context: authenticateToken as any
+    }
+  ));
+
+  if(process.env.NODE_ENV === 'production'){
+    app.use(express.static(path.join(__dirname, '../client/dist')));
+
+    app.get('*', (_req: Request, res: Response) => {
+      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+    });
+  }
+
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}/graphql`);
+  })
+};
+
+const oldServer = () => {
+  const app = express();
+  const PORT = process.env.PORT || 3001;
+
+  app.use(express.urlencoded({ extended: true }));
+  app.use(express.json());
+
+  // if we're in production, serve client/build as static assets
+  if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+
+  app.use(routes);
+
+  dbOld.once('open', () => {
+    app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  });
+};
+
+startApolloServer();
+
