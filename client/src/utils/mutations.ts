6be711daa6135faf4add5e532a6ddb7d6e111408
Joshua <joshuapruitt6484@gmail.com>
2024-10-11 22:56:26 -0700

    added mutations and queries to client

---
+++ client/src/utils/mutations.ts
@@ -0,0 +1,49 @@
+import { gql } from "@apollo/client";
+
+export const LOGIN_USER = gql`
+    mutation login($email: String!, $password: String!){
+        login(email: $email, password: $password){
+            token
+            user {
+                _id
+                username
+                email
+            }
+        }
+    }
+`;
+
+export const ADD_USER = gql`
+    mutation Mutation($input: UserInput!){
+        addUser(input: $input){
+            user {
+                email
+                username
+                _id
+
+            }
+            token
+        }
+    }
+`;
+
+export const SAVE_BOOK = gql`
+    mutation saveBook($input: BookInput!){
+        saveBook(input: $input){
+            _id
+            bookId
+            title
+            authors
+            description
+            image
+            link
+        }
+    }
+`;
+
+export const DELETE_BOOK = gql`
+    mutation deleteBook($bookId: String!){
+        deleteBook(bookId: $bookId){
+        }
+    }
+`
